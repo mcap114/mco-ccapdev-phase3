e8c4eb73f8e62bc5842ed9cb1c2382c461699954
mcap114 <132965794+mcap114@users.noreply.github.com>
2024-04-05 23:51:59 +0800

    edit mongouri

--- app.js
+++ app.js
@@ -82,7 +82,8 @@ server.use(express.static('public'));
 
 // mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/cofeedDB');
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI);
 
 mongoose.connection.on('connected', () => {
   console.log('\nDatabase connected successfully\n');
